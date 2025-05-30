d6cc383f02082b402ac10de9a7029cb931274aa6
Prakhar <115631222+Prakhar-gith@users.noreply.github.com>
2025-05-30 10:48:43 +0530

    Create 234-PalindromeLinkedList

---
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.java
@@ -0,0 +1,31 @@
+// Last updated: 5/30/2025, 10:48:43 AM
+
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode secondHalf = reverse(slow);
+        ListNode firstHalf = head;
+        while (secondHalf != null) {
+            if (firstHalf.val != secondHalf.val) return false;
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
