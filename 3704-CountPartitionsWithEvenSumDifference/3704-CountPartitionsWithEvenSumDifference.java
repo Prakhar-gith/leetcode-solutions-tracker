a03a88be6406bf0b26f14ea35a5f2f2a470b0fe2
Prakhar <115631222+Prakhar-gith@users.noreply.github.com>
2025-08-30 18:56:23 +0530

    Create 3704-CountPartitionsWithEvenSumDifference

---
+++ 3704-CountPartitionsWithEvenSumDifference/3704-CountPartitionsWithEvenSumDifference.java
@@ -0,0 +1,26 @@
+// Last updated: 8/30/2025, 6:56:22 PM
+class Solution {
+    public int countPartitions(int[] nums) {
+        int n = nums.length;
+        int totalSum = 0;
+
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+        int count = 0;
+        int righSum=0;
+
+        for (int i = 0; i < n - 1; i++) {
+            leftSum += nums[i]; 
+            int rightSum = totalSum - leftSum; 
+
+            if ((leftSum+rightSum)%2==0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
