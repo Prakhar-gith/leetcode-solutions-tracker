1c5b5e0a0097ef5fe8ff7821b0b4f2b3e945b42b
Prakhar <115631222+Prakhar-gith@users.noreply.github.com>
2025-08-30 18:56:50 +0530

    Create 3445-LexicographicallyMinimumStringAfterRemovingStars

---
+++ 3445-LexicographicallyMinimumStringAfterRemovingStars/3445-LexicographicallyMinimumStringAfterRemovingStars.java
@@ -0,0 +1,42 @@
+// Last updated: 8/30/2025, 6:56:50 PM
+class Solution {
+    public String clearStars(String s) {
+        int n = s.length();
+        // Deques to store indices of each letter
+        @SuppressWarnings("unchecked")
+        Deque<Integer>[] pos = new ArrayDeque[26];
+        for (int i = 0; i < 26; i++) {
+            pos[i] = new ArrayDeque<>();
+        }
+        boolean[] deleted = new boolean[n];
+        
+        // First pass: record letter positions, and process stars
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '*') {
+                // Find smallest letter available to the left
+                for (int ch = 0; ch < 26; ch++) {
+                    if (!pos[ch].isEmpty()) {
+                        // Remove the rightmost occurrence to minimize lex order
+                        int delIdx = pos[ch].removeLast();
+                        deleted[delIdx] = true;
+                        break;
+                    }
+                }
+            } else {
+                // Record this letter's index
+                pos[c - 'a'].addLast(i);
+            }
+        }
+        
+        // Build the answer: skip '*' and deleted letters
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c != '*' && !deleted[i]) {
+                ans.append(c);
+            }
+        }
+        return ans.toString();
+    }
+}
