67a63eeb971fffe71c85c8080289694c0b102588
Prakhar <115631222+Prakhar-gith@users.noreply.github.com>
2025-09-02 22:51:30 +0530

    Create 3025-FindtheNumberofWaystoPlacePeopleI

---
+++ 3025-FindtheNumberofWaystoPlacePeopleI/3025-FindtheNumberofWaystoPlacePeopleI.java
@@ -0,0 +1,40 @@
+// Last updated: 9/2/2025, 10:51:30 PM
+class Solution {
+    public int numberOfPairs(int[][] points) {
+        int n = points.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                int[] A = points[i];
+                int[] B = points[j];
+
+                if (A[0] <= B[0] && A[1] >= B[1]) {
+                    boolean isRectangleEmpty = true;
+                    
+                    for (int k = 0; k < n; k++) {
+                        if (k == i || k == j) {
+                            continue;
+                        }
+
+                        int[] C = points[k];
+                        
+                        if (A[0] <= C[0] && C[0] <= B[0] && B[1] <= C[1] && C[1] <= A[1]) {
+                            isRectangleEmpty = false;
+                            break;
+                        }
+                    }
+
+                    if (isRectangleEmpty) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
