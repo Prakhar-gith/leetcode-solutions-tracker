bf4e51fb6281e82085a289fa352f3acf0f0ef2d4
Prakhar <115631222+Prakhar-gith@users.noreply.github.com>
2025-09-12 22:06:20 +0530

    Create 3227-VowelsGameinaString

---
+++ 3227-VowelsGameinaString/3227-VowelsGameinaString.java
@@ -0,0 +1,12 @@
+// Last updated: 9/12/2025, 10:06:20 PM
+class Solution {
+    public boolean doesAliceWin(String s) {
+        for (char c : s.toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
