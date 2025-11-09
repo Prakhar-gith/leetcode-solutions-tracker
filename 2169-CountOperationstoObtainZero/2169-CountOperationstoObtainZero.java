423259acc37d2c8bd8d0efdc81a58387569bd996
Prakhar <115631222+Prakhar-gith@users.noreply.github.com>
2025-11-09 18:40:25 +0530

    Create 2169-CountOperationstoObtainZero

---
+++ 2169-CountOperationstoObtainZero/2169-CountOperationstoObtainZero.java
@@ -0,0 +1,15 @@
+// Last updated: 11/9/2025, 6:40:23 PM
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int operations = 0;
+        while(num1 > 0 && num2 > 0){
+            if(num1 >= num2){
+                num1 -= num2;
+            } else {
+                num2 -= num1;
+            }
+            operations++;
+        }
+        return operations;
+    }
+}
