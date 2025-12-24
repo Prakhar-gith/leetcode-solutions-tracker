9127a7608e602eba9b1e4905faac94ff44ab37b1
Prakhar <115631222+Prakhar-gith@users.noreply.github.com>
2025-12-24 16:11:22 +0530

    Create

---
+++ problem-1766572880789/problem-1766572880789.java
@@ -0,0 +1,15 @@
+// Last updated: 12/24/2025, 4:11:20 PM
+1class Solution {
+2    public int minimumBoxes(int[] apple, int[] capacity) {
+3        int s=0;
+4        for(int a:apple) s+= a;
+5        Arrays.sort(capacity);
+6        int c= 0;
+7        int i= capacity.length- 1;
+8        while(s > 0){
+9            s-= capacity[i--];
+10            c++;
+11        }
+12        return c;
+13    }
+14}
